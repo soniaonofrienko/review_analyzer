2cd22d61f71e7a2fb2b26aa650cb2f70e72e5008
Соня <saonofrienko@edu.hse.ru>
2025-12-06 23:55:12 +0300

    fix temp.py

--- temp.py
+++ temp.py
@@ -1,6 +1,13 @@
-from src.core import load_reviews_from_csv
+from src.core import load_reviews_from_csv, analyze_reviews
 
+# загрузка данных
 reviews = load_reviews_from_csv("data/sample_reviews.csv")
 print(f"Было загружено {len(reviews)} отзывов")
-for review in reviews[:3]:
-    print(review)
+
+# анализ
+analyzed = analyze_reviews(reviews)
+
+# выводим примеры
+for i, r in enumerate(analyzed[:3]):
+    print(f"\n{r['review_text']}")
+    print(f"Полярность: {r['polarity']:.2f}, Метка: {r['sentiment_label']}")
